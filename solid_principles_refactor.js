0336e66516232b78e5041c74bc93365e832c6e34
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:18:05 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for AI-News-Briefing.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:18:04Z'
+});
